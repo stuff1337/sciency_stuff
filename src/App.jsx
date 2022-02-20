b7c9f724c73890780f974bf4c076f6cc8b6cb384
caspa <you@example.com>
2022-02-20 20:36:19 +0300

    firebase groundwork

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,6 @@ import styles from './App.module.scss'
 import { CustomProvider } from 'rsuite'
 
 const App = () => {
-
-    // const [isLoading, setIsLoading] = useState(false)
-    // const [user, setUser] = useState(undefined)
-
-    // const fetchUserAxios = () => axios('https://randomuser.me/api/')
-    
-    // const onStartupEffect = async() => {
-    //     setIsLoading(true)
-    //     try {
-    //         const { data } = await fetchUserAxios()
-    //         setUser(data.results[0])
-    //     } catch(err) {
-    //         console.warn(err)
-    //     } finally {
-    //         setIsLoading(false)
-    //     }
-    // }
-  
     return (
         <CustomProvider theme='high-contrast'>
             <div className={styles.wrapper}>
--- src/utils/scripts.js
+++ src/utils/scripts.js
@@ -1,5 +1,7 @@
 import { USER_HISTORY_FIELD, DEFAULT_LS_VALUE } from "./"
 
+const { REACT_APP_FIREBASE_KEY: API_KEY } = process.env
+
 const getFromLS = (field) => JSON.parse(localStorage.getItem(field))
 const setLSField = (field, value) => 
     localStorage.setItem(field, JSON.stringify(value))
@@ -16,16 +18,3 @@ export const getUserHistory = () => {
 export const setUserHistory = (value) => {
     setLSField(USER_HISTORY_FIELD, value)
 }
-
-// export const getIsUserAgreementSigned = () => getFromLS(USER_AGREEMENT_FIELD)
-// export const getIsUserTestFinished = () => getFromLS(USER_TEST_FINISHED_FIELD)
-// export const getIsUserEmailProvided = () => getFromLS(USER_EMAIL_PROVIDED)
-
-// export const setIsUserAgreementSigned = (value) => 
-//     setLSField(USER_AGREEMENT_FIELD, value)
-
-// export const setIsUserTestFinished = (value) =>
-//     setLSField(USER_TEST_FINISHED_FIELD, value)
-
-// export const setIsUserEmailProvided = (value) =>
-//     setLSField(USER_EMAIL_PROVIDED, value)
