eb6749023b4c96a22aa9b2add47e4a66834b9102
stuff1337 <100073551+stuff1337@users.noreply.github.com>
2022-02-20 20:00:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,7 @@ import UserProvider from './components/UserProvider'
 import styles from './App.module.scss'
 import { CustomProvider } from 'rsuite'
 
-const App = () => {
-
-    // const [isLoading, setIsLoading] = useState(false)
-    // const [user, setUser] = useState(undefined)
-
-    // const fetchUserAxios = () => axios('https://randomuser.me/api/')
-    
-    // const onStartupEffect = async() => {
-    //     setIsLoading(true)
-    //     try {
-    //         const { data } = await fetchUserAxios()
-    //         setUser(data.results[0])
-    //     } catch(err) {
-    //         console.warn(err)
-    //     } finally {
-    //         setIsLoading(false)
-    //     }
-    // }
-  
+const App = () => {  
     return (
         <CustomProvider theme='high-contrast'>
             <div className={styles.wrapper}>
